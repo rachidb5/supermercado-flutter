78abfd12164a8140cf9f3138093bb97d39973ada
rachidb5 <66842598+rachidb5@users.noreply.github.com>
2021-01-14 18:17:40 -0300

    Update menuLateral.dart

--- lib/widgets/menuLateral.dart
+++ lib/widgets/menuLateral.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:mercado/tabs/productTab.dart';
-//import '../models/user_model.dart';
-import '../screens/loginScreen.dart';
+import '../models/userModel.dart';
+import '../screens/login.dart';
 import '../tiles/drawerTile.dart';
-//import 'package:scoped_model/scoped_model.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 class MenuLateral extends StatelessWidget {
   final PageController pageController;
@@ -46,12 +46,35 @@ class MenuLateral extends StatelessWidget {
                 ),
               ),
               //Divider(),
-              FlatButton(
-                  onPressed: () {
-                    Navigator.of(context).push(
-                        MaterialPageRoute(builder: (context) => LoginScreen()));
-                  },
-                  child: Text('Cadaste-se ou faça login')),
+              ScopedModelDescendant<Usermodel>(
+                  builder: (context, child, model) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Olá, ${!model.isLoggedIn() ? "" : model.userData["name"]}",
+                      style: TextStyle(
+                          fontSize: 18.0, fontWeight: FontWeight.bold),
+                    ),
+                    GestureDetector(
+                      child: Text(
+                        !model.isLoggedIn() ? "Entre ou cadastre-se >" : "Sair",
+                        style: TextStyle(
+                            color: Theme.of(context).primaryColor,
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      onTap: () {
+                        if (!model.isLoggedIn())
+                          Navigator.of(context).push(
+                              MaterialPageRoute(builder: (context) => Login()));
+                        else
+                          model.signOut();
+                      },
+                    ),
+                  ],
+                );
+              }),
               DrawerTile(Icons.list, "Produtos", pageController, 0),
               DrawerTile(Icons.location_on, "Lojas", pageController, 2),
               DrawerTile(
