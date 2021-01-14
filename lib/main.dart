31e614a830e5deeb5303ef25c5d5d761a7b78d07
rachidb5 <66842598+rachidb5@users.noreply.github.com>
2021-01-14 18:15:54 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import './screens/homeScreen.dart';
 import 'package:scoped_model/scoped_model.dart';
 import './models/userModel.dart';
+import './models/cartModel.dart';
 
 void main() {
   runApp(MyApp());
@@ -19,13 +20,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScopedModel<Usermodel>(
       model: Usermodel(),
-      child: MaterialApp(
-          title: 'Flutter Demo',
-          theme: ThemeData(
-              primarySwatch: Colors.blue,
-              primaryColor: Color.fromARGB(255, 4, 125, 141)),
-          debugShowCheckedModeBanner: false,
-          home: HomeScreen()),
+      child: ScopedModelDescendant<Usermodel>(builder: (context, child, model) {
+        return ScopedModel<Cartmodel>(
+          model: Cartmodel(model),
+          child: MaterialApp(
+              title: "Mercantil",
+              theme: ThemeData(
+                  primarySwatch: Colors.blue,
+                  primaryColor: Color.fromARGB(255, 4, 125, 141)),
+              debugShowCheckedModeBanner: false,
+              home: HomeScreen()),
+        );
+      }),
     );
   }
 }
